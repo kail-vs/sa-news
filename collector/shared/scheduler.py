84c0803af553696cdbe1b9f4e54388b488584e22
kail-vs <keiosinl@gmail.com>
2026-02-06 12:00:46 +0530

    Changed scheduler

--- collector/shared/scheduler.py
+++ collector/shared/scheduler.py
@@ -2,9 +2,12 @@ def get_apis_to_run(minute: int):
     apis = []
 
     if minute % 15 == 0:
-        apis.extend(["newsapi", "newsdatahub"])
+        apis.extend(["newsdatahub"])
 
-    if minute % 10 == 0:
+    if minute % 2 == 0:
+        apis.extend(["newsapi"])
+
+    if minute % 8 == 0:
         apis.append("newsdata")
 
     return apis
