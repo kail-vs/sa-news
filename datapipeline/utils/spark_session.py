43d8960faf9210e25d20b072b1115698ff1697a4
kail-vs <keiosinl@gmail.com>
2026-02-13 17:27:51 +0530

    Implemented language detection on aggregated textual data in gold layer

--- datapipeline/utils/spark_session.py
+++ datapipeline/utils/spark_session.py
@@ -1,8 +1,11 @@
 from pyspark.sql import SparkSession
 from delta import configure_spark_with_delta_pip
+import sys
 
 
 def get_spark_session(app_name="SA-News"):
+
+    python_exec = sys.executable
 
     builder = (
         SparkSession.builder
@@ -15,11 +18,26 @@ def get_spark_session(app_name="SA-News"):
         .config("spark.sql.shuffle.partitions", "4")
         .config("spark.driver.memory", "4g")
 
-        .config("spark.sql.extensions",
-                "io.delta.sql.DeltaSparkSessionExtension")
-        .config("spark.sql.catalog.spark_catalog",
-                "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        .config("spark.pyspark.python", python_exec)
+        .config("spark.pyspark.driver.python", python_exec)
 
+        .config(
+            "spark.sql.extensions",
+            "io.delta.sql.DeltaSparkSessionExtension"
+        )
+        .config(
+            "spark.sql.catalog.spark_catalog",
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog"
+        )
+
+        .config(
+            "spark.hadoop.fs.file.impl",
+            "org.apache.hadoop.fs.LocalFileSystem"
+        )
+        .config(
+            "spark.hadoop.fs.file.impl.disable.cache",
+            "true"
+        )
     )
 
     spark = configure_spark_with_delta_pip(builder).getOrCreate()
